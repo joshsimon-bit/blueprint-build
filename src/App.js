468deff0408f0ebe56b15650b656a58d08ff0409
joshsimon-bit <83602394+joshsimon-bit@users.noreply.github.com>
2021-10-03 00:11:42 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Button from "../components/NeonButton";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>Welcome to my Portfolio ;-) ...</h2>
+      <Button />
+    </div>
+  );
+}
